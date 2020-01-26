f50e9a31523e6e60678c30f8b2bc4c2edc3ef821
mneller <martin@ellermeier.net>
2020-01-26 18:39:05 +0100

    Lint corrections

--- src/app/app-config.service.ts
+++ src/app/app-config.service.ts
@@ -48,21 +48,21 @@ export class AppConfigService {
               `Haftungsansprüche gegen den Autor wegen Schäden materieller oder immaterieller Art, welche aus dem Zugriff
            oder der Nutzung bzw. Nichtnutzung der veröffentlichten Informationen, durch Missbrauch der Verbindung oder durch
            technische Störungen entstanden sind, werden ausgeschlossen.`,
-              `Alle Angebote sind unverbindlich. Der Autor behält es sich ausdrücklich vor, Teile der Seiten oder das gesamte 
+              `Alle Angebote sind unverbindlich. Der Autor behält es sich ausdrücklich vor, Teile der Seiten oder das gesamte
            Angebot ohne besondere Ankündigung zu verändern, zu ergänzen, zu löschen oder die Veröffentlichung
            zeitweise oder endgültig einzustellen.`],
             nonliabilityLinks: `
-                Verweise und Links auf Webseiten Dritter liegen ausserhalb unseres Verantwortungsbereichs. Es wird jegliche Verantwortung 
-                für solche Webseiten abgelehnt. Der Zugriff und die Nutzung solcher Webseiten erfolgen auf eigene Gefahr des jeweiligen 
+                Verweise und Links auf Webseiten Dritter liegen ausserhalb unseres Verantwortungsbereichs. Es wird jegliche Verantwortung
+                für solche Webseiten abgelehnt. Der Zugriff und die Nutzung solcher Webseiten erfolgen auf eigene Gefahr des jeweiligen
                 Nutzers.
               `,
             copyright: `
-                Die Urheber- und alle anderen Rechte an Inhalten, Bildern, Fotos oder anderen Dateien auf dieser Website, gehören 
-                 ausschliesslich dem Inhaber der Website oder den speziell genannten Rechteinhabern. Für die Reproduktion 
+                Die Urheber- und alle anderen Rechte an Inhalten, Bildern, Fotos oder anderen Dateien auf dieser Website, gehören
+                 ausschliesslich dem Inhaber der Website oder den speziell genannten Rechteinhabern. Für die Reproduktion
                 jeglicher Elemente ist die schriftliche Zustimmung des Urheberrechtsträgers im Voraus einzuholen.
                `,
-            sourceLink: "https://www.swissanwalt.ch/impressum-generator.aspx",
-            sourceText: "Impressum-Generator von SwissAnwalt",
+            sourceLink: 'https://www.swissanwalt.ch/impressum-generator.aspx',
+            sourceText: 'Impressum-Generator von SwissAnwalt',
           };
         this.settings = { appName: 'Hugo',  impressum: impressumData};
         resolve(true);
