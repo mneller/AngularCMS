8d4c4f8e514ddacff44c1884e395ca25f2706854
mneller <martin@ellermeier.net>
2020-08-09 18:28:44 +0200

    Fixed testing problem in HomeComponent

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -5,18 +5,29 @@ import { HomeComponent } from './home.component';
 import {AppConfigService} from '../app-config.service';
 import {MockAppConfigService} from '../mock-app-config.service';
 import {Title} from '@angular/platform-browser';
+import {MockStore, provideMockStore} from '@ngrx/store/testing';
+import {AppState, initialAppState} from '../app.reducer';
+import {Store} from '@ngrx/store';
 
 describe('HomeComponent', () => {
+  let store: MockStore<AppState>;
+  const initialState = {
+      title: 'toBeSet',
+    } as AppState;
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ HomeComponent ],
-      providers: [{provide: AppConfigService, useClass: MockAppConfigService}, Title],
+      providers: [
+        provideMockStore<AppState>({ initialState }),
+        // , Title
+      ],
       schemas: [ NO_ERRORS_SCHEMA ],
     })
     .compileComponents();
+    // store = TestBed.get(Store);
   }));
 
   beforeEach(() => {
